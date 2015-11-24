59a6d1ef177e2eb53fcd9f8f70d59a42aac702b9
Thiago Durante Pires <thdurante@gmail.com>
2015-11-24 17:29:18 -0200

    #2549 Adequação ao contrato de dados e requisições para a página “login.html”

--- Code-DD-UFG/Front-End/sdd-front-end/js/login/login.js
+++ Code-DD-UFG/Front-End/sdd-front-end/js/login/login.js
@@ -4,8 +4,41 @@ app.controller('loginController', function($scope){
 
 	$scope.submitForm = function(isValid){
 		if(isValid){
-			console.log("Tudo ok!");
-			// Abre a requisição para o back-end
+
+			console.log("Username: " + $("#username").val());
+			//console.log("Senha: " + $("#senha").val());
+			
+            var senhaDigest = sha1($("#senha").val());
+            var timestamp = $.now()/1000;
+            var auth_pass = sha1(senhaDigest+timestamp);
+
+            console.log("Auth_pass: " +auth_pass);
+
+            $.ajax({
+	            url: 'http://private-anon-33b49d585-sddufg.apiary-mock.com/sessions',
+	            type: 'POST',
+	            dataType: 'json',
+	            data: {
+	                'username': $("#username").val(),
+	                'auth_pass': auth_pass
+	            },
+	            success: function(response){
+	            	var session_token = response['session_token'];
+	            	console.log("Resposta da API: " +session_token);
+
+	            	sessionStorage.setItem('session_token', session_token);
+	            	console.log("Checando tamanho do sessionStorage: " +sessionStorage.length);
+	            	console.log("Recuperando o session_token do sessionStorage: " +sessionStorage.getItem('session_token'));
+
+	            	alert("Login realizado com sucesso! [session_token: " +session_token +"]");
+	            	window.location.href = "index.html";
+	            }, 
+	            statusCode: {
+	            	403: function(response){
+	            		console.log(response['status'] +": " +response['message']);
+	            	}
+	            }
+        	});
 		}
 	};
 });
