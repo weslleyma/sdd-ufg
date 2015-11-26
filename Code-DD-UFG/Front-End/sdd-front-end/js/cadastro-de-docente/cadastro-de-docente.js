5a8bf0224f0d1b03029ea5b2a63766de5d45c108
Thiago Durante Pires <thdurante@gmail.com>
2015-11-26 00:37:56 -0200

    #2529 Adequação ao contrato de dados e requisições para a página “cadastro-de-docente.html”

--- Code-DD-UFG/Front-End/sdd-front-end/js/cadastro-de-docente/cadastro-de-docente.js
+++ Code-DD-UFG/Front-End/sdd-front-end/js/cadastro-de-docente/cadastro-de-docente.js
@@ -4,8 +4,101 @@ app.controller('cadastroDocenteController', function($scope){
 
 	$scope.submitForm = function(isValid){
 		if(isValid){
-			console.log("Tudo ok!");
-			// Abre a requisição para o back-end
+			
+			console.log("Nome: " + $("#nomeDocente").val());
+			console.log("Matrícula: " + $("#matricula").val());
+			console.log("Data de ingresso: " + $("#dataIngresso").val());
+			console.log("Username: " + $("#username").val());
+			console.log("E-mail: " + $("#email").val());
+			console.log("Senha: " + $("#senha").val());
+
+			var token = sessionStorage.getItem("session_token");
+			console.log("Token: " +token);
+
+			$.ajax({
+	            url: 'http://private-e6e9d-sddufg.apiary-mock.com/teachers',
+	            type: 'POST',
+	            dataType: 'json',
+	            headers: {
+					"Session-Token": token,
+					"Content-Type": 'application/json'
+	            },
+	            data: {
+	                'name': $("#nomeDocente").val(),
+				    'registry': $("#matricula").val(),
+				    'url_lattes': "",
+				    'date_entry': $("#dataIngresso").val(),
+				    'formation': "",
+				    'workload': "",
+				    'about': "",
+				    'rg': "",
+				    'cpf': "",
+				    'birth_date': ""
+	            },
+	            success: function(response){
+	            	console.log("Success ajax1");
+	            }, 
+	            statusCode: {
+	            	201: function(response){
+	            		console.log("201");
+	            		console.log(response);
+	            		//alert(xhr.getResponseHeader('Location'));
+	            	},
+	            	400: function(response){
+            			console.log(response['status'] +": " +response['responseJSON']['message']);
+            			alert(response['status'] +": " +response['responseJSON']['message']);
+	            	},
+	            	403: function(response){
+            			console.log(response['status'] +": " +response['responseJSON']['message']);
+            			alert(response['status'] +": " +response['responseJSON']['message']);
+	            	}
+	            },
+	            complete: function(xhr){
+					if(xhr.getResponseHeader('Location')){
+						
+						var location = xhr.getResponseHeader('Location');
+						console.log("Location: " +location);
+
+						// Pega o id do docente
+						var arr = location.split('/');
+						console.log("Array: " +arr[2]);
+
+						var senhaDigest = sha1($("#senha").val());
+						console.log("SenhaDigest: " +senhaDigest);
+
+						$.ajax({
+				            url: 'http://private-e6e9d-sddufg.apiary-mock.com/users',
+				            type: 'POST',
+				            dataType: 'json',
+				            headers: {
+						    	"Session-Token": token,
+						    	"Content-Type": 'application/json'
+						    },
+						    data: {
+						    	'username': $("#username").val(),
+						    	'password': senhaDigest,
+						    	'email': $("#email").val(),
+						    	'teacher_id': arr[2]
+						    },
+				            success: function(response){
+				            	console.log("Success ajax2");
+				            }, 
+				            statusCode: {
+				            	201: function(response){
+				            		console.log("201");
+	            					console.log(response);
+				            	},
+				            	400: function(response){
+				            		console.log(response['status'] +": " +response['responseJSON']['message']);
+				            		alert(response['status'] +": " +response['responseJSON']['message']);
+				            	}
+				            }
+				    	});
+					}
+	            }
+        	});
+
+			
 		}
 	};
 });
