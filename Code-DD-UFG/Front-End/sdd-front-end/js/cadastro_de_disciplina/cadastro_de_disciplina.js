df10cb90591cfe014226bce779ca8ed1dd4c01e4
Romulo Borges Crispi <fulanux@gmail.com>
2015-11-29 23:27:11 -0200

    Submissão parcial dos entregáveis. 2/3 submetidos

---
+++ Code-DD-UFG/Front-End/sdd-front-end/js/cadastro_de_disciplina/cadastro_de_disciplina.js
@@ -0,0 +1,89 @@
+var app = angular.module('cadastroDisciplina', []);
+
+app.controller('cadastroDisciplinaController', function($scope){
+
+	$scope.submitForm = function(isValid){
+		console.log("Código: " + $("#codigoDaDisciplina").val());
+		console.log("Disciplina: " + $("#nomeDaDisciplina").val());
+		console.log("Carga horária: " + $("#seletorDeCargaHoraria").val());
+		console.log("Curso: " + $("#seletorDoSemestre").val());
+
+		var token = sessionStorage.getItem("session_token");
+		console.log("Token: " + token);
+
+		$.ajax({
+            url: 'http://private-e6e9d-sddufg.apiary-mock.com/grades',
+            type: 'POST',
+            dataType: 'json',
+            headers: {
+				"Session-Token": token,
+				"Content-Type": 'application/json'
+            },
+            data: {
+            	"name": $("#nomeDaDisciplina").val(),
+            	"course_id": $("#seletorDoSemestre").val(),
+            	"knowledge_id": 1
+            },
+            success: function(response){
+            	console.log("Success ajax1");
+            }, 
+            statusCode: {
+            	201: function(response){
+            		console.log("201");
+            		console.log(response);
+            		//alert(xhr.getResponseHeader('Location'));
+            	},
+            	400: function(response){
+        			console.log(response['status'] +": " +response['responseJSON']['message']);
+        			alert(response['status'] +": " +response['responseJSON']['message']);
+            	},
+            	403: function(response){
+        			console.log(response['status'] +": " +response['responseJSON']['message']);
+        			alert(response['status'] +": " +response['responseJSON']['message']);
+            	}
+            },
+            complete: function(xhr){
+				if(xhr.getResponseHeader('Location')){
+					
+					var location = xhr.getResponseHeader('Location');
+					console.log("Location: " + location);
+
+					var arr = location.split('/');
+					console.log("Array: " + arr[2]);
+
+					var senhaDigest = sha1($("#senha").val());
+					console.log("SenhaDigest: " + senhaDigest);
+
+					$.ajax({
+			            url: 'http://private-e6e9d-sddufg.apiary-mock.com/users',
+			            type: 'POST',
+			            dataType: 'json',
+			            headers: {
+					    	"Session-Token": token,
+					    	"Content-Type": 'application/json'
+					    },
+					    data: {
+					    	'username': $("#username").val(),
+					    	'password': senhaDigest,
+					    	'email': $("#email").val(),
+					    	'teacher_id': arr[2]
+					    },
+			            success: function(response){
+			            	console.log("Success ajax2");
+			            }, 
+			            statusCode: {
+			            	201: function(response){
+			            		console.log("201");
+            					console.log(response);
+			            	},
+			            	400: function(response){
+			            		console.log(response['status'] +": " +response['responseJSON']['message']);
+			            		alert(response['status'] +": " +response['responseJSON']['message']);
+			            	}
+			            }
+			    	});
+				}
+            }
+    	});
+	};
+});
